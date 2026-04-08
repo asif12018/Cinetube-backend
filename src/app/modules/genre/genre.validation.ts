28ceb94019c5fd5045208e891da3cd82bc3729d8
asif12018 <asif.request@gmail.com>
2026-04-08 15:38:12 +0600

    minor zod error fixed

--- src/app/modules/genre/genre.validation.ts
+++ src/app/modules/genre/genre.validation.ts
@@ -1,21 +1,22 @@
 import z from "zod";
 
-
 export const createGenre = z.object({
-    body: z.object({
-        name: z.string().min(3,"Genre name must be at least 3 characters long"),
-        slug: z.string().min(3,"Genre slug must be at least 3 characters long")
-    })
-})
+  name: z.string().min(3, "Genre name must be at least 3 characters long"),
+  slug: z.string().min(3, "Genre slug must be at least 3 characters long"),
+});
 
 export const updateGenre = z.object({
-    body: z.object({
-        name: z.string().min(3,"Genre name must be at least 3 characters long").optional(),
-        slug: z.string().min(3,"Genre slug must be at least 3 characters long").optional()
-    })
-})
+  name: z
+    .string()
+    .min(3, "Genre name must be at least 3 characters long")
+    .optional(),
+  slug: z
+    .string()
+    .min(3, "Genre slug must be at least 3 characters long")
+    .optional(),
+});
 
 export const GenreValidation = {
-    createGenre,
-    updateGenre
-}
+  createGenre,
+  updateGenre,
+};
